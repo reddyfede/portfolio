62c9aced177d24d5a97b7bc91cafde252697a9e6
reddy <reddyfede@gmail.com>
2023-08-28 23:17:39 +0200

    set up pages routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,17 @@
-import {Header, Footer} from "../"
+import { Route, Routes } from "react-router-dom";
+import {Header, Footer} from "../../components"
+import {Home, About, Projects} from "../../pages"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <h1>APP</h1>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
       <Footer />
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
     return(
         <div className="Header">
-            <h1>Header</h1>
+            <Link to="/">
+                <div>HOME</div>
+            </Link>
+            <Link to="/about">
+                <div>ABOUT</div>
+            </Link>
+            <Link to="/projects">
+                <div>PROJECTS</div>
+            </Link>
         </div>
     )
 }
