a5c1ada08810e154bb5f7d4db1089a776ddb7d44
reddy <reddyfede@gmail.com>
2023-09-01 20:34:08 +0200

    add responsive style

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -6,21 +6,23 @@ export default function Projects() {
         <div id="projects">
             <hr/>
             <h1>Projects</h1>
-            {ProjectList.map((project, idx)=>(
-                <div className={project.class + " card"} key={idx}>
-                    <img src={project.image} alt={project.name}/>
-                    <p>{project.about}</p>
-                    <div>
-                        {project.tech.map((t,id)=>(
-                            <h5 className={project.class} key={id}>{t}</h5>
-                        ))}
-                    </div>
-                    <div>
-                        <a href={project.github} className={project.class}>Github Link</a>
-                        <a href={project.link} className={project.class}>Live Link</a>
-                    </div>
-                </div> 
-            ))}
+            <div className="projContainer">
+                {ProjectList.map((project, idx)=>(
+                    <div className={project.class + " card"} key={idx}>
+                        <img src={project.image} alt={project.name}/>
+                        <p>{project.about}</p>
+                        <div>
+                            {project.tech.map((t,id)=>(
+                                <h5 className={project.class} key={id}>{t}</h5>
+                            ))}
+                        </div>
+                        <div>
+                            <a href={project.github} className={project.class} target="_blank" rel="noreferrer">Github Link</a>
+                            <a href={project.link} className={project.class} target="_blank" rel="noreferrer">Live Link</a>
+                        </div>
+                    </div> 
+                ))}
+            </div>
         </div>
     )
 }
