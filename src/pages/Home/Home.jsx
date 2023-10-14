0598d038abad16cde1d2936d35d0929a608e638c
reddy <reddyfede@gmail.com>
2023-10-14 20:13:50 +0200

    add slide in animation

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,18 @@
-import foto from "../../images/profile.jpg"
-import "./Home.css"
-import {Contacts} from "../../components"
+import foto from '../../images/profile.jpg';
+import './Home.css';
+import { Contacts } from '../../components';
 
 export default function Home() {
-    return(
-        <div id="home">
-            <img src={foto} alt="profilePic"/>
-            <div>
-                <h1>federicoRossi</h1>
-                <p>Software Engineer</p>
-                <Contacts/>
-            </div>
+  return (
+    <div id='home'>
+      <img src={foto} alt='profilePic' />
+      <div>
+        <h1>federicoRossi</h1>
+        <p>Software Engineer</p>
+        <div className='contacts'>
+          <Contacts />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
