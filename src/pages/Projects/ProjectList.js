8ff32e756c23c2cd28d0e68d03a9768117152e0a
reddy <reddyfede@gmail.com>
2023-10-14 20:58:08 +0200

    adjust transform dimension

--- src/pages/Projects/ProjectList.js
+++ src/pages/Projects/ProjectList.js
@@ -16,7 +16,7 @@ const ProjectList = [
     {
         name: "event.ly",
         about: "event.ly is a map-based web application for finding and creating events to connect with people in your local community. Created in 10 days as a pair-programming project for the General Assembly Software Engineer Immersive Bootcamp.",
-        tech: ["Node.js","Express","React","Mongoose","MongoDB","Tailwind", "JavaScript"],
+        tech: ["Node","Express","React","MongoDB","Tailwind", "Leaflet", "BingMap API", "Unsplash API", "JavaScript"],
         image: eventlyGif,
         github: "https://github.com/reddyfede/event-ly",
         link:"https://event-ly.netlify.app/",
@@ -25,7 +25,7 @@ const ProjectList = [
     {
         name: "Little Italy Pizza",
         about: "Little Italy Pizza is a MEN-stack web application for a fictitious pizza shop. Created as a week-long pair programming project at GA Software Engineering Immersive Bootcamp.",
-        tech: ["Node.js","Express","Mongoose","MongoDB","Bootstrap", "JavaScript"],
+        tech: ["Node","Express","MongoDB","Bootstrap", "JavaScript"],
         image: litteitalypizzaGif,
         github: "https://github.com/reddyfede/little-italy-pizza",
         link:"https://littleitaly-e2a8e59785f8.herokuapp.com/",
