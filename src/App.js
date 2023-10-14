700f38e9045154a547bea6243fcb4a57c892f99f
reddy <reddyfede@gmail.com>
2023-10-14 21:47:46 +0200

    add background effect

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div class="bg"></div>
+      <div class="bg bg2"></div>
+      <div class="bg bg3"></div>
       <Navbar />
-      <Home />
-      <Projects />
-      <About />
+        <Home />
+        <Projects />
+        <About />
       <Footer />
     </div>
   );
